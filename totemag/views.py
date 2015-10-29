6c82fb148b48b3bdc759b4fa8cf67158ee591be9
Devon Warren <devon.warren@gmail.com>
2015-10-28 23:42:40 -0400

    mailchimp functionality!

--- totemag/views.py
+++ totemag/views.py
@@ -1,8 +1,12 @@
 from django.template.loader import get_template
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from articles.models import Article
 from staff.models import Staff
+from django.conf import settings
+import mailchimp
+from django.contrib import messages
 
 
 def homepage(request):
@@ -15,7 +19,7 @@ def homepage(request):
 
 
 def about(request):
-    staff = Staff.objects.all()
+    staff = Staff.objects.filter(featured=True)
     t = get_template('about.html')
     html = t.render(RequestContext(request, {
             'staff': staff,
@@ -27,3 +31,17 @@ def videos(request):
     t = get_template('videos.html')
     html = t.render(RequestContext(request))
     return HttpResponse(html)
+
+
+def subscribe(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        try:
+            api = mailchimp.Mailchimp(settings.MAILCHIMP_API_KEY)
+            api.lists.subscribe('770a3ce5b5', {'email': email})
+            messages.add_message(request, messages.INFO,
+                "Thanks for subscribing! You should have a confirmation email.")
+        except mailchimp.ListAlreadySubscribedError as detail:
+            messages.add_message(request, messages.INFO,
+                "You are already subscribed!")
+        return redirect('/')

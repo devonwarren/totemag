dab2bcfa4852c5b6201bc0b7b1bd6dbe04d806c9
Devon Warren <devon.warren@gmail.com>
2015-11-04 09:30:41 -0500

    fix homepage grid

--- articles/views.py
+++ articles/views.py
@@ -8,9 +8,14 @@ from django.http import HttpResponse
 def list_articles(request, slug=None):
     if slug:
         category = get_object_or_404(Category, slug=slug)
-        articles = Article.objects.filter(published=True, category=category)
+        articles = Article.objects.filter(
+            published=True,
+            category=category
+            ).order_by('-published_date')
     else:
-        articles = Article.objects.filter(published=True)
+        articles = Article.objects.filter(
+            published=True
+            ).order_by('-published_date')
     categories = Category.objects.filter(parent=None)
     t = get_template('homepage.html')
     html = t.render(RequestContext(request, {

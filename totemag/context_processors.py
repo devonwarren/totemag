19648aa90c77175e4fc98ee57d9e316126a98fa3
Devon Warren <devon.warren@gmail.com>
2016-03-26 12:28:24 -0400

    make navigation dynamic

--- totemag/context_processors.py
+++ totemag/context_processors.py
@@ -1,9 +1,31 @@
 from articles.models import Category
+from django.core.cache import cache
 
 
-def categories(request):
-    all_categories = Category.objects.all()
+def navigation(request):
+    if not cache.get('navigation'):
+        categories = Category.objects.filter(parent=None).select_related()
+        nav = []
+
+        for cat in categories:
+            temp = {
+                'name': cat.name,
+                'slug': cat.slug,
+                'children': []
+            }
+            child = cat.children.all()
+
+            if child:
+                for c in child:
+                    temp['children'].append({
+                            'name': c.name,
+                            'slug': c.slug
+                        })
+
+            nav.append(temp)
+
+        cache.set('navigation', nav)
 
     return {
-        'categories': all_categories,
+        'navigation': cache.get('navigation'),
     }
--- totemag/settings/local.py
+++ totemag/settings/local.py
@@ -4,8 +4,8 @@ DEBUG = True
 
 TEMPLATES[0]['OPTIONS']['debug'] = True
 
-#INSTALLED_APPS += ("debug_toolbar", )
+INSTALLED_APPS += ("debug_toolbar", )
 INTERNAL_IPS = ("127.0.0.1",)
-#MIDDLEWARE_CLASSES += \
-#    ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+MIDDLEWARE_CLASSES += \
+    ("debug_toolbar.middleware.DebugToolbarMiddleware", )
 

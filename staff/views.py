295a5f040aa384697a3a6df081e1989383657412
Devon Warren <devon.warren@gmail.com>
2016-04-10 18:52:29 -0400

    add image attribution articles to staff view page

--- staff/views.py
+++ staff/views.py
@@ -1,16 +1,26 @@
 from staff.models import Staff
 from articles.models import Article
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 from datetime import date, timedelta
 
 
 def staff_view(request, slug):
     staff = get_object_or_404(Staff, slug=slug)
-    articles = Article.objects.filter(publisher=staff)
+
+    articles = Article.objects.filter(
+        Q(publisher=staff, published=True) |
+        Q(image_attribution=staff, published=True))
+
     popular_articles = Article.objects.filter(
-        published=True,
-        published_date__gte=(date.today() - timedelta(days=31)),
-        publisher=staff.id
+        Q(
+            published=True,
+            published_date__gte=(date.today() - timedelta(days=31)),
+            publisher=staff.id) |
+        Q(
+            published=True,
+            published_date__gte=(date.today() - timedelta(days=31)),
+            image_attribution=staff.id)
         ).order_by('-count')[:6]
 
     return render(

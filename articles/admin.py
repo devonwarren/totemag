7c9d07a31543070de233f1073f76753a639fd908
Devon Warren <devon.warren@gmail.com>
2016-04-11 13:43:44 -0400

    hide count and publish after options in admin

--- articles/admin.py
+++ articles/admin.py
@@ -8,11 +8,24 @@ class SlideshowInline(admin.StackedInline):
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'published', 'publisher', 'category')
-    search_fields = ['title',]
+    list_display = (
+        'title', 'published', 'published_date', 'publisher', 'category')
+    search_fields = ['title', ]
     list_filter = ('published', 'category')
     inlines = [SlideshowInline]
     exclude = ['slideshow_images', ]
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title', 'published', 'image', 'image_attribution',
+                'body', 'category', 'published_date', 'publisher')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': (
+                'publish_after', 'count'),
+        }),
+    )
 
 
 class CategoryAdmin(admin.ModelAdmin):

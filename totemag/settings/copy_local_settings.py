8e4a69034f8221d6b57741fe296a6126ddbfc573
Devon Warren <devon.warren@gmail.com>
2016-06-01 20:53:59 -0400

    add new local_settings copy

---
+++ totemag/settings/copy_local_settings.py
@@ -0,0 +1,26 @@
+"""
+
+Copy this file to local_settings.py and edit the values. 
+
+Make sure you set the DJANGO_SETTINGS_MODULE environment variable to local_settings as well.
+
+
+"""
+# Local environment?
+# from totemag.settings.local import *
+# Production environment?
+# from totemag.settings.production import *
+
+# Override database settings
+#DATABASES['default']['USER'] = 'root'
+#DATABASES['default']['PASSWORD'] = 'password'
+
+
+# super secret salt hash
+#SECRET_KEY = ''
+
+# api key to get into mailchimp
+#MAILCHIMP_API_KEY = ''
+
+# email credentials
+#EMAIL_HOST_PASSWORD = ''
